75f329bd9b6a0359ab7821b6684ba783f15a02c7
Pradip Bhattacharjya <93419916+pradipbhattacharjya@users.noreply.github.com>
2023-02-24 04:09:32 +0000

    Variables_input in js program

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId =721626
+let accountEmail ="pradipbhattacharjya1997@gmail.com"
+var accountPassword ="1234"
+accountCity = "Bajkul"
+let accoundState;
+
+// accoundtId =2 //not allowed
+accountEmail ="hshdjh@gmail.com"
+accountPassword ="2121212121"
+accountCity = "kolkata"
+
+console.log(accountId);
+/*
+   prefer not to use var
+   because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accoundState])
