cac38973f25b239e72f1e4aa462660c344c3d512
Pradip Bhattacharjya <93419916+pradipbhattacharjya@users.noreply.github.com>
2023-02-24 14:18:47 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";//treat all Js code as newer version
+
+//alert(3+3)/ We are using nodejs,not browser
+
+console.log(3
+    
+    +
+    3)//code readabiliy should be high
+
+    console.log("pradip")
+
+let name = "prdip"
+let age = 23
+let isLoggedIn =false
+let state 
+
+//numbr => 2 to power 53
+// bignt
+//string =>""
+//bolean => true/false
+//null => standalone value
+// undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof "padip");
+
+console.log(typeof undefined);//undefined
+
+console.log(typeof null);//object
