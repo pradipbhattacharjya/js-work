d3562dffb93862b235e68c828cceed9c92da1ab4
Pradip Bhattacharjya <93419916+pradipbhattacharjya@users.noreply.github.com>
2023-02-25 18:34:21 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+//# primitive
+
+//  7 types: String, Number, Boolearn, null, undefinde, Symbol, BingInt
+
+const score =100
+const scorevalue = 100.3
+
+const isoggedIn = false
+const outsideTemp= null
+let userEmail;
+
+const id =symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+// Reference (Non primitive)
+
+// Array, Objects,Functions
+
