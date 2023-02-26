1a7a262b0ef72a8742f9de479f80b2d4f19b63f0
Pradip Bhattacharjya <93419916+pradipbhattacharjya@users.noreply.github.com>
2023-02-26 16:35:52 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name ="padip"
+const repoCount =50
+
+//console.log(name+repoCount+"value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('pradipB')
+console.log(gameName[3]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
