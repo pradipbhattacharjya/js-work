71294bf1273c7e7202e4aea8ab66a258e98ae7ff
Pradip Bhattacharjya <93419916+pradipbhattacharjya@users.noreply.github.com>
2023-02-26 03:41:14 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -31,5 +31,31 @@ const myFunction = function(){
 
 }
 
-console.log(typofbigNumber);
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
+console.log(typeof anotherId);
 
+
+
+/*
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+
+
+
+
+
+*/
